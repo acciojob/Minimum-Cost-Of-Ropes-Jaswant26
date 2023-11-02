15f94bb1e29154cf3a1a21a1dde5e2957c838580
Jaswant Singh Rawat <71880220+Jaswant26@users.noreply.github.com>
2023-11-02 17:47:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ function calculateMinCost() {
   let string = document.getElementById("rope-lengths").value;
 	let arr = string.split(",");
 	let finalValue = 0;
-  }
     while (arr.length > 1) {
       arr.sort((a, b) => {return (a - b)});
-      let value = parseInt.(arr.shift());
-      let value1 = parseInt.(arr.shift());
+      let value = parseInt(arr.shift());
+      let value1 = parseInt(arr.shift());
       let mainValue = value+value1;
 		finalValue = finalVlue + mainValue;
 		arr.push(mainValue);
     }
-    let result = document.getElementById(result);
+    let result = document.getElementById("result");
 result.innerText = finalValue;
 return finalValue;
 }
